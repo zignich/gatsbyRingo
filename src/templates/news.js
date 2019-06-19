7e1a20f943cafd08ec43753839d6ddaadcb38a32
zignich <zignich.ds@gmail.com>
2019-06-19 11:31:18 +0600

    added social media share buttons on news page

--- src/templates/news.js
+++ src/templates/news.js
@@ -8,6 +8,7 @@ import Sidebar from '../components/Sidebar/index';
 export const query = graphql`
   query($uid: String!) {
     prismicBlog(uid: { eq: $uid }) {
+      uid
       data {
         publication_date(locale: "ru", formatString: "MMMM Do YYYY")
         title {
@@ -21,6 +22,11 @@ export const query = graphql`
         }
       }
     }
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
   }
 `
 
@@ -30,6 +36,7 @@ const News = props => {
   const content = props.data.prismicBlog.data.content.html
   const publishedDate = props.data.prismicBlog.data.publication_date
   const imageUrl = props.data.prismicBlog.data.news_cover.url
+  const pageUrl = props.data.site.siteMetadata.siteUrl + "/news/" + props.data.prismicBlog.uid
 
   return (
     <Layout>
@@ -51,6 +58,24 @@ const News = props => {
             <div className="news">
               <div dangerouslySetInnerHTML={{ __html: content }} />
             </div>
+
+            <div className="row socialLinksBox">
+                <strong className="orangeColor">
+                  Поделиться новостью:
+                </strong>
+                <div>
+                  <a className="socialLinks" href={"https://www.facebook.com/sharer/sharer.php?u=" + pageUrl}>
+                  <span class="icon fa-facebook"></span>
+                  </a>
+                  <a className="socialLinks" href="https://twitter.com/intent/tweet">
+                  <span class="icon fa-twitter"></span>
+                  </a>
+                  <a className="socialLinks" href={"http://vk.com/share.php?url=" + pageUrl}>
+                  <span class="icon fa-vk"></span>
+                  </a>
+                </div>
+            </div>
+
             <br />
             <div className="row">
               <div className="col-12 col-12-mobile">
